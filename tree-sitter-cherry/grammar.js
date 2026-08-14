ec572b1afe6e6387c3d75a8b37ceba300e66cb3b
Jon Jennemann <j@jonf.se>
2026-08-14 23:19:41 +0200

    fixed some examples. bug fixes

--- tree-sitter-cherry/grammar.js
+++ tree-sitter-cherry/grammar.js
@@ -37,7 +37,8 @@ module.exports = grammar({
       'break', 'const', 'continue', 'else', 'err', 'for', 'false',
       'fn', 'if', 'null', 'module', 'out', 'println', 'print',
       'return', 'remove', 'true', 'var', 'while', 'len', 'in',
-      "rm", "exists", "sleep", "time", "key", "clr", "global"
+      "rm", "exists", "sleep", "time", "key", "clr", "global",
+      "elif"
     ),
 
     // Based on your switch statements for operators
